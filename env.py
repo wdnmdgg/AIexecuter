953f08d57766aa59ca7921e0fe3e3071f7d7869b
Guanzhuo Qiao <gqiao@stevens.edu>
2019-08-26 18:56:26 -0400

    environment object

--- env.py
+++ env.py
@@ -1,9 +1,4 @@
-import numpy
 import shift
-import tensorflow
-import datetime
-import shift
-import pandas as pd
 import numpy as np
 import time
 class env:
@@ -69,29 +64,6 @@ class env:
     def get_record(self):
         return self.record
 
-
-
-
-class env:
-
-    def __init__(self, ticker, start_price, total_size, starting_time, time_limit, buy_side):
-        self.ticker = ticker
-        self.start_price = start_price
-        self.total_size = total_size
-        self.starting_time = starting_time
-        self.time_limit = time_limit
-        self.buy_side = buy_side
-
-    def get_states(self, num):  # 'num' is the observation number
-
-        # 1. current market price
-        bp = shift.Trader.getBestPrice(self.ticker)
-        if self.buy_side:
-            current_price = bp.getBidPrice()
-        else:
-            current_price = bp.getAskPrice()
-
-        # 2.
 
 
 
