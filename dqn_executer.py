56196bc1e8782a25787f8bc60d83d97b0e7b574d
Ang Li <ali24@stevens.edu>
2019-10-01 18:44:39 -0400

    Structure finished

--- dqn_executer.py
+++ dqn_executer.py
@@ -1,11 +1,12 @@
 from env import Env
 from agent import LSTMAgent
+from pool import SimpleReplayPool
 import shift
 import numpy as np
 import tensorflow as tf
 import tensorflow.keras as keras
 import Q_function as Qf
-from datetime import datetime
+import time
 
 GAMMA = 0.95
 EPSILON = 0.1
@@ -14,6 +15,9 @@ trader = shift.Trader("username")
 symbol = None
 commission = None
 sess = tf.Session()
+execute_time = 3600
+exe_times = 120
+exe_interval = execute_time / exe_times
 
 
 def main():
@@ -29,13 +33,24 @@ def main():
                       GAMMA=GAMMA,
                       EPSILON=EPSILON,
                       learning_rate=0.001)
+    pool = SimpleReplayPool(max_pool_size=1000,
+                            pop_size=100)
 
     for i in range(EPISODES):
-        pass
-
-
-
-
+        # Deal with the initialization for each episode
+        for j in range(exe_times):
+            time.sleep(exe_interval)
+# ---------------------------------------------
+            if j == 0:
+                env.get_obs()
+            else:
+                env.step()
+            agent.get_action()
+            agent.save_buffer()
+            pool.add_sample()
+        pool.random_batch()
+        agent.train()
+# ---------------------------------------------
 
 
 if __name__ == '__main__':
